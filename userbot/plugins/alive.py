64ff0f7682d5fc20d41a3cd57dac8d52a684d5d5
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-10-20 20:13:08 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,5 +1,5 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-#IMG CREDITS: @WhySooSerious
+#IMG CREDITS: @xark047
 import asyncio
 from telethon import events
 from uniborg.util import admin_cmd
@@ -7,7 +7,7 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/9f89cca30e7289fe9399f.jpg"
-pm_caption = "`Apun jinda h sur :` **ONLINE**\n\n"
+pm_caption = "`I'm online:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **1.15.0**\n`Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
@@ -15,10 +15,7 @@ pm_caption += "**Current Branch** : `master`\n"
 pm_caption += "**Ionix os** : `2.0`\n"
 pm_caption += "**Current Sat** : `IoNix-2.25`\n"
 pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "Copyright : By [matrixhackz@Github](GitHub.com/matrixhack)\n"
-pm_caption += " [Deploy Ionix useebot](https://telegra.ph/Ionix-06-15)"
+pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n" 
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def Ionix(alive):
