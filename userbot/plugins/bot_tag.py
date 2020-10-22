87eb30539f44af343a21c9f1f9e8269e4b842bce
dark12knight <70634949+dark12knight@users.noreply.github.com>
2020-10-22 20:53:09 +0530

    Update bot_tag.py

--- userbot/plugins/bot_tag.py
+++ userbot/plugins/bot_tag.py
@@ -5,10 +5,10 @@ from telethon.tl.types import (
     User
 )
 from telethon.utils import get_display_name
-from userbot.utils import bot_on_cmd sudo_cmd, edit_or_reply
+from userbot.utils import borg_on_cmd sudo_cmd, edit_or_reply
 from userbot.uniborgConfig import Config
 
-@bot.on(events.NewMessage(
+@borg.on(events.NewMessage(
     incoming=True,
     blacklist_chats=Config.UB_BLACK_LIST_CHAT,
     func=lambda e: (
